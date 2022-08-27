ad0e2573b620832f405310cb4796f6331a055fab
Jordan Rabideau <jmr8990@g.rit.edu>
2022-08-27 15:55:38 -0400

    remove express from repo

--- server/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.send('Senior Development Project Homepage!')
-})
-
-module.exports = router
