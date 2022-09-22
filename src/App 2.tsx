6940e743c49f306d69265b6badd8c8d8aeab4d6c
emc3424 <35965473+emc3424@users.noreply.github.com>
2022-09-21 21:37:48 -0400

    All three charts show up

---
+++ src/App 2.tsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar />
+     </div>
+   );
+}
+
+export default App;
