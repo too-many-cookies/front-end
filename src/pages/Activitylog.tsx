19ce951584047bf3544fdc2a11d737e60ab6d8f2
Jordan Rabideau <jmr8990@g.rit.edu>
2022-10-07 15:29:36 -0400

    remove duplicate nav from activity log

--- src/pages/Activitylog.tsx
+++ src/pages/Activitylog.tsx
@@ -2,12 +2,11 @@ import Navbar from "../components/Navbar";
 import React from "react";
 
 function ActivityLog() {
-    return (
-        <div className="ActivityLog">
-            <Navbar />
-            <h1>ActivityLog</h1>
-        </div>
-    );
+  return (
+    <div className="ActivityLog">
+      <h1>ActivityLog</h1>
+    </div>
+  );
 }
 
 export default ActivityLog;
