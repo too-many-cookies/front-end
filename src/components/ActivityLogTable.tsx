f6ac13e1667bd94073b2db768a2b71bd6d10c9aa
Jordan Rabideau <jmr8990@g.rit.edu>
2022-10-27 20:58:12 -0400

    change timezone of dates on tables

--- src/components/ActivityLogTable.tsx
+++ src/components/ActivityLogTable.tsx
@@ -1,45 +1,51 @@
 import React from "react";
 import "../styles/classTable.css";
-import { LogTableProps, RecentLog, Activity, ActivityLogTableProps } from "../interfaces";
+import {
+  LogTableProps,
+  RecentLog,
+  Activity,
+  ActivityLogTableProps,
+} from "../interfaces";
 
 function formatDate(timestamp: Date) {
-    const date = new Date(timestamp);
-    const day = date.toLocaleDateString();
-    const time = date.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-    });
+  const date = new Date(timestamp);
+  const day = date.toLocaleDateString();
+  const time = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "UTC",
+  });
 
-    return `${day} ${time}`;
+  return `${day} ${time}`;
 }
 
 function ActivityLogTable(logs: ActivityLogTableProps) {
-    return (
-        <div className="classTable">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Student</th>
-                        <th>ID</th>
-                        <th>Action</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {logs.logs.map((log: Activity) => (
-                        <tr>
-                            <td>{log.name}</td>
-                            <td>{log.username}</td>
-                            <td>
-                                {log.successful === "Y" ? "Successful Login" : "Failed Login"}
-                            </td>
-                            <td>{formatDate(log.timestamp)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="classTable">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Student</th>
+            <th>ID</th>
+            <th>Action</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.logs.map((log: Activity) => (
+            <tr>
+              <td>{log.name}</td>
+              <td>{log.username}</td>
+              <td>
+                {log.successful === "Y" ? "Successful Login" : "Failed Login"}
+              </td>
+              <td>{formatDate(log.timestamp)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ActivityLogTable;
