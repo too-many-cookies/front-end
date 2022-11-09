df8632ece01f813d3effdfb6500ac7807b1380d2
Jordan <jordanmrabideau@gmail.com>
2022-11-08 20:29:04 -0500

    update remaining tables

--- src/components/ClassTable.tsx
+++ src/components/ClassTable.tsx
@@ -2,6 +2,27 @@ import React from "react";
 import "../styles/classTable.css";
 import { ClassInfo, Classes } from "../interfaces";
 import { useNavigate } from "react-router-dom";
+import DataTable from 'react-data-table-component';
+
+type TableClass = {
+  name: string
+  class_id: number
+  class_section: string
+  total_student_count: number
+  students_instantiated: number
+}
+
+function formatClasses(classes: Array<ClassInfo>) {
+  return classes.map(c => {
+    return {
+      name: c.name,
+      class_id: c.class_id,
+      class_section: `${c.class_code}.${c.class_section_number}`,
+      total_student_count: c.total_student_count,
+      students_instantiated: c.students_instantiated
+    }
+  })
+}
 
 function ClassTable({ list }: Classes) {
   const navigate = useNavigate();
@@ -9,32 +30,41 @@ function ClassTable({ list }: Classes) {
     const path = `/classes/${id}`;
     navigate(path);
   };
+
+  const columns = [
+    {
+      name: "Class",
+      selector: (row: TableClass) => row.name,
+      sortable: true
+    },
+    {
+      name: "Section",
+      selector: (row: TableClass) => row.class_section,
+      sortable: true
+    },
+    {
+      name: "# of Students",
+      selector: (row: TableClass) => row.total_student_count,
+      sortable: true
+    },
+    {
+      name: "Students Instantiated",
+      selector: (row: TableClass) => row.students_instantiated,
+      sortable: true
+    }
+  ]
+  const data = formatClasses(list)
+  const styles = {
+    rows: {
+      highlightOnHoverStyle: {
+        backgroundColor: '#d0d3d4',
+        cursor: 'pointer'
+      }
+    }
+  }
   return (
     <div className="classTable">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Class</th>
-            <th>Section</th>
-            <th># of Students</th>
-            <th>Students Instantiated</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((c, key) => (
-            <tr
-              onClick={() => routeChange(c.class_id)}
-              className="class-row"
-              key={key}
-            >
-              <td>{c.name}</td>
-              <td>{`${c.class_code}.${c.class_section_number}`}</td>
-              <td>{c.total_student_count}</td>
-              <td>{c.students_instantiated}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable columns={columns} customStyles={styles} data={data} onRowClicked={(row: TableClass, event) => routeChange(row.class_id)} />
     </div>
   );
 }
--- src/components/StudentTable.tsx
+++ src/components/StudentTable.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "../styles/classTable.css";
-import { Students } from "../interfaces";
+import { Students, Student } from "../interfaces";
+import DataTable from 'react-data-table-component';
+
+type TableStudent = {
+  name: string
+  username: string
+  instantiated: string
+  last_sign_in: string
+}
 
 function formatDate(date: Date) {
   const datetime = new Date(date);
@@ -14,30 +22,58 @@ function formatDate(date: Date) {
   return `${day} ${time}`;
 }
 
+function formatStudent(students: Array<Student>) {
+  return students.map(s => {
+    return {
+      name: s.name,
+      username: s.username,
+      instantiated: s.instantiated ? "YES" : "NO",
+      last_sign_in: formatDate(s.last_sign_in)
+    }
+  })
+}
+
 function StudentTable({ list }: Students) {
+  const columns = [
+    {
+      name: "Student",
+      selector: (row: TableStudent) => row.name,
+      sortable: true
+    },
+    {
+      name: "Username",
+      selector: (row: TableStudent) => row.username,
+      sortable: true
+    },
+    {
+      name: "Instantiated",
+      selector: (row: TableStudent) => row.instantiated,
+      sortable: true
+    },
+    {
+      name: "Last Activity",
+      selector: (row: TableStudent) => row.last_sign_in,
+      sortable: true,
+      sortFunction: (a: TableStudent, b: TableStudent) => {
+        const d1 = new Date(a.last_sign_in)
+        const d2 = new Date(b.last_sign_in)
+
+        if (d1 < d2) {
+          return -1
+        }
+    
+        if (d1 > d2) {
+          return 1
+        }
+    
+        return 0
+      }
+    }
+  ]
+  const data = formatStudent(list)
   return (
     <div className="classTable">
-      <table className="table">
-        <thead>
-          {/* Create a heading column for every key in the prop obj */}
-          <tr>
-            <th>Student</th>
-            <th>Username</th>
-            <th>Instantiated</th>
-            <th>Last Activity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((c) => (
-            <tr>
-              <td>{c.name}</td>
-              <td>{c.username}</td>
-              <td>{c.instantiated ? "YES" : "NO"}</td>
-              <td>{c.last_sign_in ? formatDate(c.last_sign_in) : "N/A"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
